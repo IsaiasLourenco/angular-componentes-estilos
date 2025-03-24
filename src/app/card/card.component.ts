28eb282e641be3dd32d33f0519942a00bf8c3f34
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-24 14:58:21 -0300

    First commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: false,
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100,
+    }
+  }
+}
