e9494622495b4c4e801aa6e3333387e421beab70
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-25 01:34:31 -0300

    Novo botão cancelar

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-card-button',
   standalone: false,
   templateUrl: './card-button.component.html',
+  template: `
+      <div class="card-button">TESTE</div>
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string,
+  preco: number,
+}
+
 @Component({
   selector: 'app-card',
   standalone: false,
@@ -7,10 +16,12 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = {
+  //@ts-ignore
+  plano: IPlano = {
     infos: {
       tipo: 'Simples',
       preco: 100,
-    }
+    },
+  };  
   }
-}
+
