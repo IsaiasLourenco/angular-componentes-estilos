0fd016c493d3974a61867b06e2db40109f3b4a2e
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-26 01:43:30 -0300

    Segunda parte de componentes

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,21 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-card-button-cancel',
   standalone: false,
   templateUrl: './card-button-cancel.component.html',
-  styleUrl: './card-button-cancel.component.scss',
-  styles: [
-    `
-    .card-cancel-button {
-      font-size: 16px;
-      text-align: center;
-      margin-top: 10px;
-      border-radius: 25px;
-      background-color: blue;
-      cursor: pointer;
-      font-weight: bold;
-      padding: 10px;
-    }
-    `
-  ],
+  styleUrl: './card-button-cancel.component.scss'
 })
 export class CardButtonCancelComponent {
 
