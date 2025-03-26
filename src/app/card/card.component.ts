7d884edfecb643d30c4abda0e427f8bee9ec0b19
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-26 17:06:35 -0300

    Final da seção 3

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 interface IPlano {
   infos: IInfos;
@@ -13,7 +13,8 @@ interface IInfos {
   selector: 'app-card',
   standalone: false,
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent {
   //@ts-ignore
